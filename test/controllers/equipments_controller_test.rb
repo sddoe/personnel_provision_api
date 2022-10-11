d6eef08bfc696f1e1d44abec58c1ca68487fde2e
Sergio David Perdomo Rivera <sergio.931129@gmail.com>
2022-10-11 07:46:38 -0500

    Add unit tests

--- test/controllers/equipments_controller_test.rb
+++ test/controllers/equipments_controller_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class EquipmentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @equipment = equipment(:one)
+  end
+
+  test "should get index" do
+    get equipments_url, as: :json
+    assert_response :success
+  end
+
+  test "should create equipment" do
+    assert_difference("Equipment.count") do
+      post equipments_url, params: { equipment: { serial: 'ABC1', name: 'iPhone', description: 'Smartphone' } }, as: :json
+    end
+
+    assert_response :success
+  end
+
+  test "should show equipment" do
+    get equipment_url(@equipment), as: :json
+    assert_response :success
+  end
+
+  test "should update equipment" do
+    patch equipment_url(@equipment), params: { equipment: { name: 'OtherString' } }, as: :json
+    assert_response :success
+  end
+
+  test "should destroy equipment" do
+    assert_difference("Equipment.count", -1) do
+      delete equipment_url(@equipment), as: :json
+    end
+
+    assert_response :success
+  end
 end
--- test/controllers/owners_controller_test.rb
+++ test/controllers/owners_controller_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class OwnersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @owner = owners(:one)
+  end
+
+  test "should get index" do
+    get owners_url, as: :json
+    assert_response :success
+  end
+
+  test "should create owner" do
+    assert_difference("Owner.count") do
+      post owners_url, params: { owner: { name: 'Mike', email: 'mike@email.com' } }, as: :json
+    end
+
+    assert_response :success
+  end
+
+  test "should show owner" do
+    get owner_url(@owner), as: :json
+    assert_response :success
+  end
+
+  test "should update owner" do
+    patch owner_url(@owner), params: { owner: { name: 'Yorick', email: 'yorick@email.com' } }, as: :json
+    assert_response :success
+  end
+
+  test "should destroy owner" do
+    assert_difference("Owner.count", -1) do
+      delete owner_url(@owner), as: :json
+    end
+
+    assert_response :success
+  end
 end
